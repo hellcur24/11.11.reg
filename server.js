2d6c63e1d990db21783f37014f543bf21daff8a6
Opilane TTHK <opilane@tthk.ee>
2020-11-18 11:23:12 +0200

    regerstr

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('console');
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+//mongoose.connect('mongodb:/localhost:27017/secrectDB', 
+//{useNewUrlParser: true, useUnifiedTopology: true});
+
+
+app.get('/',(req, res) => {
+    res.render('home');
+});
+
+app.get('/register',(req,res) =>{
+    res.render('register');
+});
+
+app.get('/secrets',(req,res)=>{
+    res.render('secrets');
+});
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+})
